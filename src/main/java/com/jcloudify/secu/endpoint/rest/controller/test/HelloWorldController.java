261195c4fb4289523d009c72693f808ce3b10948
andydevlabs <avotiana@mail.hei.school>
2025-02-12 13:45:26 +0300

    feat: implement HelloWorldController

---
+++ src/main/java/com/jcloudify/secu/endpoint/rest/controller/test/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.jcloudify.secu.endpoint.rest.controller.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String HelloWorld() {
+        return "Hello World";
+    }
+}
