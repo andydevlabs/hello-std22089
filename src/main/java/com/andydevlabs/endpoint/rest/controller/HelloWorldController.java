dbcd0619363138f4d108c21820c6587be9ff0cb9
andydevlabs <avotiana@mail.hei.school>
2025-02-12 13:48:40 +0300

    refactor: change HelloWorldController location

--- src/main/java/com/andydevlabs/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/andydevlabs/endpoint/rest/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.jcloudify.secu.endpoint.rest.controller.test;
+package com.andydevlabs.endpoint.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
